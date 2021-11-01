130ce65089143e8f29bfed0a86adc3c5c6fa467c
lucaslins0035 <lucaslinssouza@gmail.com>
2021-11-01 20:01:21 -0300

    Add aruco readings in +BT

--- bbot_navigation/scripts/behavior_tree.py
+++ bbot_navigation/scripts/behavior_tree.py
@@ -5,6 +5,8 @@
 ##############################################################################
 
 import functools
+import tf
+from tf.transformations import quaternion_from_euler
 import py_trees
 import py_trees_ros
 import py_trees.console as console
@@ -32,6 +34,59 @@ class SetBuffer(py_trees.blackboard.SetBlackboardVariable):
         self.variable_value = self.__Buffer_To_Goal()
         super(SetBuffer, self).initialise()
 
+class MarkerListener(py_trees.behaviour.Behaviour):
+    def __init__(self, name,tf1,tf2,xy=(0,0),yaw=0):
+        self.tf1 = tf1
+        self.tf2 = tf2
+        self.tf_listener = tf.TransformListener()
+        self.BB = py_trees.blackboard.Blackboard()
+        self.tf_msg = geometry_msgs.PoseStamped()
+        self.xy = xy
+        self.yaw = yaw
+        
+        super(MarkerListener,self).__init__(name)
+    
+    def initialise(self):
+        self.tf_msg = geometry_msgs.PoseStamped()
+        self.tf_msg.header.frame_id = "map"
+        self.tf_msg.header.stamp = rospy.Time.now()
+        
+    def update(self):
+        try:
+            (trans,rot) = self.tf_listener.lookupTransform(self.tf1, self.tf2, rospy.Time(0))
+            self.tf_msg.pose.position.x = trans[0] + self.xy[0]
+            self.tf_msg.pose.position.y = trans[1] + self.xy[1]
+            self.tf_msg.pose.position.z = 0.0
+            rot2quaternion = quaternion_from_euler(0,0,self.yaw)       
+            self.tf_msg.pose.orientation.x = rot2quaternion[0]
+            self.tf_msg.pose.orientation.y = rot2quaternion[1]
+            self.tf_msg.pose.orientation.z = rot2quaternion[2]
+            self.tf_msg.pose.orientation.w = rot2quaternion[3]
+            self.BB.set('target_pose',self.tf_msg)
+            self.BB.set('pose_buffer',self.tf_msg)
+            return py_trees.Status.SUCCESS
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return py_trees.Status.RUNNING
+        
+class CheckMarkerPosition(py_trees.behaviour.Behaviour):
+    def __init__(self, name=""):
+        self.msg1 = geometry_msgs.PoseStamped()
+        self.msg2 = geometry_msgs.PoseStamped()
+        self.BB = py_trees.blackboard.Blackboard()
+        
+        super(CheckMarkerPosition, self).__init__(name)
+    
+    def initialise(self):
+        self.msg1 = self.BB.get("target_pose")
+        
+    def update(self):
+        self.msg2 = self.BB.get("target_pose")
+        trans_diff = ((self.msg1.pose.position.x - self.msg2.pose.position.x)**2 + (self.msg1.pose.position.y - self.msg2.pose.position.y)**2)**0.5
+        if trans_diff > 0.1:
+            return py_trees.Status.SUCCESS
+        else:
+            return py_trees.Status.RUNNING
+        
 ##############################################################################
 # Actions
 ##############################################################################
@@ -147,7 +202,7 @@ def create_root():
     bt = py_trees.composites.Sequence("BT")
 
     get_goal = py_trees.composites.Selector("GetGoal")
-    get_buffer = py_trees.composites.Selector("GetBuffer")
+    # get_buffer = py_trees.composites.Selector("GetBuffer")
     apply_buffer = py_trees.composites.Sequence("ApplyBuffer")
     chk_preemp = py_trees.composites.Selector("CheckPreemption")
     fallback = py_trees.composites.Selector("Fallback")
@@ -169,6 +224,7 @@ def create_root():
                                                                     topic_name="/move_base_simple/goal",
                                                                     topic_type=geometry_msgs.PoseStamped,
                                                                     blackboard_variables = {'target_pose': None,'pose_buffer': None})
+    chk_marker_pos = py_trees.meta.success_is_running(py_trees.meta.running_is_failure(CheckMarkerPosition))(name="CheckArucoPos")
     
     GetPath_seq = py_trees.composites.Selector("GetPathSeq")
     get_path = GetPath(name="GetPath",
@@ -196,17 +252,19 @@ def create_root():
         variable_name='event_cancel_button',
         expected_value=True
     )
+    
+    marker2bb = MarkerListener("Aruco2BB","/map","/aruco_id_0",(2,1),1.57)
 
     clr_goal3 = py_trees.blackboard.ClearBlackboardVariable(name="ClearGoal", variable_name="target_pose")
     clr_buffer3 = py_trees.blackboard.ClearBlackboardVariable(name="ClearGoalBuffer", variable_name="pose_buffer")
 
     # Compose tree
-    bt_root.add_children([cancel2bb, bt])
+    bt_root.add_children([cancel2bb,marker2bb, bt])
     bt.add_children([get_goal, chk_preemp])
-    chk_preemp.add_children([cancelling, set_preempt,fallback])
+    chk_preemp.add_children([cancelling, set_preempt, chk_marker_pos,fallback])
     cancelling.add_children([is_cancel_requested, clr_goal3, clr_buffer3])
-    get_goal.add_children([have_goal, get_buffer])
-    get_buffer.add_children([apply_buffer,new_goal])
+    get_goal.add_children([have_goal, apply_buffer, new_goal])
+    # get_buffer.add_children([apply_buffer,new_goal])
     apply_buffer.add_children([have_buffer,set_buffer])
     navigate.add_children([GetPath_seq, exe_path, clr_goal1,clr_buffer])
     GetPath_seq.add_children([get_path,check_getpath_result])
